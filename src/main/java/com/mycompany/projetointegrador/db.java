b40922a0ee74aca67cb4708d5ebb76fcddd94079
Luan Matos <luanmatos.ti@gmail.com>
2021-11-09 17:08:47 -0300

    projeto sagaz

--- src/main/java/com/mycompany/projetointegrador/db.java
+++ src/main/java/com/mycompany/projetointegrador/db.java
@@ -1,5 +1,25 @@
 package com.mycompany.projetointegrador;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class db {
-   
+   public static Connection connect() throws Exception{
+        //1. Carregar o Driver
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+
+        //2. Conectar no Banco
+        //jdbc:sqlserver://localhost:1433;databaseName=jdbc_test;user=utest;password=123456
+        Connection conexao = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=jdbc_test;user=teste;password=pepino");
+
+
+        //3. Retornar uma conexão para quem chamou
+        return conexao; 
+    }
+
+    public static void main(String[] args) throws Exception {
+        connect();
+        System.out.println("Conectado!");
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.mycompany.projetointegrador {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens com.mycompany.projetointegrador to javafx.fxml;
     exports com.mycompany.projetointegrador;
