d7ed1ba4a5cde68f4d7c859ad20429674ac21e1d
Luan Matos <luanmatos.ti@gmail.com>
2021-11-17 17:03:04 -0300

    adição de telas de edição

---
+++ src/main/java/com/mycompany/projetointegrador/CadastrarItemEdicaoController.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projetointegrador;
+
+import java.net.URL;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author lmato
+ */
+public class CadastrarItemEdicaoController implements Initializable {
+
+    @FXML
+    private TextField txtAutor;
+    @FXML
+    private TextField txtTitulo;
+    @FXML
+    private TextField txtEditora;
+    @FXML
+    private DatePicker dtPublicacao;
+    @FXML
+    private TextField txtPagina;
+    @FXML
+    private ComboBox comboCategoria;
+    @FXML
+    private TextField txtPreco;
+    @FXML
+    private TextField txtQtd;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        comboCategoria.getItems().add("Suspense");
+        comboCategoria.getItems().add("Romance");
+    }
+
+    @FXML
+    private void limpar(ActionEvent event) {
+        txtAutor.clear();
+        txtTitulo.clear();
+        txtEditora.clear();
+        dtPublicacao.setValue(null);
+        txtPagina.clear();
+        comboCategoria.getSelectionModel().clearSelection();
+        txtPreco.clear();
+        txtQtd.clear();
+    }
+
+    @FXML
+    private void cadastrar(ActionEvent event) {
+        String sql = "INSERT INTO produto (autor, titulo, editora, dtPublicacao, nmrPagina, categoria, preco, qtd) VALUES (?,?,?,?,?,?,?,?)";
+
+        try ( PreparedStatement ps = db.connect().prepareStatement(sql)) {
+            ps.setString(1, txtAutor.getText());
+            ps.setString(2, txtTitulo.getText());
+            ps.setString(3, txtEditora.getText());
+            ps.setDate(4, Date.valueOf(dtPublicacao.getValue()));
+            ps.setInt(5, Integer.parseInt(txtPagina.getText()));
+            ps.setString(6, comboCategoria.getSelectionModel().getSelectedItem().toString());
+            ps.setDouble(7, Double.parseDouble(txtPreco.getText()));
+            ps.setInt(8, Integer.parseInt(txtQtd.getText()));
+
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
