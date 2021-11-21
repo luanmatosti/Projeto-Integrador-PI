395f7c7d06add6f148f16cdcfaa42bde8338c988
Leonardo Lourenco <leoz_in@hotmail.com>
2021-11-21 17:34:09 -0300

    Metade da tela de Relatório. Verificar com Luan o erro entre LinhaTabelaRelatorio e RelatorioController

--- src/main/java/com/mycompany/projetointegrador/RelatorioController.java
+++ src/main/java/com/mycompany/projetointegrador/RelatorioController.java
@@ -1,6 +1,9 @@
 package com.mycompany.projetointegrador;
 
 import java.net.URL;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -52,11 +55,38 @@ public class RelatorioController implements Initializable {
 
     @FXML
     private void pesquisar(ActionEvent event) {
-        /*verificar com Luan
-        if (txt.getText().isBlank()) {
-            atualizarTabela();
-        } else {
-            pesquisarCpf();*/
-    }
+        /*verificar com Luan (conflito RelatorioController e LinhaTabelaRelatorio)*/
+String sql = "SELECT cliente.nome, pedido.idPedido,produto.id, produto.preco,"
++ "pedido.dataPedido FROM pedido "
++ "INNER JOIN cliente ON pedido.idcliente = cliente.id "
++ "INNER JOIN itemPedido ON pedido.idPedido = itemPedido.idPedido "
++ "INNER JOIN produto ON itemPedido.idProduto = produto.id ";
 
+//Verificar Luan (por Leonardo)
+/*try(PreparedStatement ps= db.connect().prepareStatement(sql)){
+ps.setDate(1, Date.valueOf(dtDe.getValue()));
+ps.setDate(2, Date.valueOf(dtAte.getValue()));
+
+ResultSet rs = ps.executeQuery();
+
+
+while (rs.next()){
+    LinhaTabelaRelatorio ltr = new LinhaTabelaRelatorio(
+            rs.getString("nome"),
+            rs.getInt("idPedido"),
+            rs.getInt("id"),
+            rs.getDouble("preco"),
+            rs.getDate("dataPedido").toLocalDate()
+    );
+    
+    tableRelatorio.getItems().add(ltr);
+    tableRelatorio.refresh();
 }
+ 
+}
+catch(Exception e){
+    e.printStackTrace();
+}*/
+    }
+}
+
